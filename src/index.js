5dd0d4f466cf446b4e5d71f8abc994522eb767c1
delovoyhomie <slavik-yakimenko@yandex.ru>
2023-09-09 20:17:49 +0300

    multy pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import './index.css'
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
